b17a2d10785a3ad8c338a1df9a204a1cadb716e3
kazuyaseki <track613armory@gmail.com>
2018-08-12 11:38:03 +0900

    pokemondbのRead周りのTypeとResolver書き終わり

---
+++ server/index.js
@@ -0,0 +1,90 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const pokemons = require('../data/Pokemon-DB/pokedex.json');
+
+const convertBaseObject = original => ({
+  attack: original.Attack,
+  defense: original.Defense,
+  hp: original.HP,
+  spAtk: original['Sp.Atk'],
+  spDef: original['Sp.Def'],
+  speed: original.Speed
+});
+
+const typeDefs = gql`
+  type Pokemon {
+    id: ID
+    base: Base
+    cname: String
+    ename: String
+    jname: String
+    skills: Skills
+    type: [String]
+  }
+
+  type Base {
+    attack: Int
+    defense: Int
+    hp: Int
+    spAtk: Int
+    spDef: Int
+    speed: Int
+  }
+
+  type Skills {
+    egg: [Int]
+    level_up: [Int]
+    tm: [Int]
+    transfer: [Int]
+    tutor: [Int]
+  }
+
+  type Skill {
+    id: ID
+    accuracy: Int
+    category: String
+    cname: String
+    ename: String
+    jname: String
+    power: Int
+    pp: Int
+    type: String
+  }
+
+  type Type {
+    cname: String
+    ename: String
+    jname: String
+  }
+
+  type Item {
+    cname: String
+    ename: String
+    jname: String
+  }
+
+  type Query {
+    pokemons: [Pokemon]
+    pokemon(id: ID): Pokemon
+  }
+`;
+
+const resolvers = {
+  Query: {
+    pokemons: () => pokemons,
+    pokemon(root, args, context, info) {
+      return pokemons.find(pokemon => Number(pokemon.id) == args.id);
+    }
+  },
+  Pokemon: {
+    base(root, args, context, info) {
+      return convertBaseObject(root.base);
+    }
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
