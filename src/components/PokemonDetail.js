0a219a21626d0877f4d17ceabbf87fc6d126ac29
kazuyaseki <track613armory@gmail.com>
2018-08-18 00:40:53 +0900

    最低限の情報追加

--- src/components/PokemonDetail.js
+++ src/components/PokemonDetail.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
+import { basename } from 'path';
 
 const PokemonDetail = ({ pokemon }) => (
   <Wrapper>
     <PokemonImage src={`/pokemonImg/${pokemon.id + pokemon.ename}.png`} />
-    <DetailPane />
+    <DetailPane>
+      <p>{pokemon.jname || pokemon.ename}</p>
+      <ul>
+        <li>こうげき: {pokemon.base.attack}</li>
+        <li>ぼうぎょ: {pokemon.base.defense}</li>
+        <li>HP: {pokemon.base.hp}</li>
+        <li>とっこう: {pokemon.base.spAtk}</li>
+        <li>とくぼう: {pokemon.base.spDef}</li>
+        <li>すばやさ: {pokemon.base.speed}</li>
+      </ul>
+    </DetailPane>
   </Wrapper>
 );
 
@@ -12,6 +23,9 @@ const Wrapper = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: rgb(194, 8, 25);
+  display: flex;
+  flex-flow: column;
+  align-items: center;
 `;
 
 const PokemonImage = styled.div`
@@ -27,7 +41,9 @@ const PokemonImage = styled.div`
 
 const DetailPane = styled.div`
   background-color: #fff;
-  border-radius: 8px;
+  border-radius: 16px;
+  width: calc(100vw - 40px);
+  margin-top: 20px;
 `;
 
 export default PokemonDetail;
